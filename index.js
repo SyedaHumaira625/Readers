84d186bde18ab300bacbfdfffe174fc63ef41dfd
SyedaHumaira625 <syedahumaira399@gmail.com>
2025-05-05 00:55:08 -0700

    created

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let navBar = document.querySelector("nav")
-let iconOne = document.querySelector("fa-bars")
-let iconTwo = document.querySelector("fa-circle-xmark")
+let iconOne = document.querySelector(".fa-bars")
+let iconTwo = document.querySelector(".fa-circle-xmark")
 
 iconOne.addEventListener("click", function() {
     navBar.style.top = "-100%"
