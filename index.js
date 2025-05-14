e4b733efba613760751e8456ddd80417ff1d4423
SyedaHumaira625 <syedahumaira399@gmail.com>
2025-05-14 12:48:11 -0700

    navigation bar created

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
-let navBar = document.querySelector("nav")
-let iconOne = document.querySelector(".fa-bars")
-let iconTwo = document.querySelector(".fa-circle-xmark")
+// let navBarContainer = document.querySelector(".nav-sect-1");
+let navBar = document.querySelector("nav");
+let iconXmark = document.querySelector("#cross-icon");
+let iconMenu = document.querySelector("#menu-icon");
 
-iconOne.addEventListener("click", function() {
-    navBar.style.top = "-100%"
-})
+iconXmark.addEventListener("click", function () {
+  navBar.style.transform = "translateY(-100%)";
+});
+
+iconMenu.addEventListener("click", function () {
+  navBar.style.transform = "translateY(0px)";
+});
+
+let heroSectImg = document.querySelectorAll(".herosect-image");
+
+let counter = 0;
+
+heroSectImg.forEach((image, index) => {
+    image.style.left = `${index * 100}%`
+});
+
+
+let slideImg = () => {
+    heroSectImg.forEach(() => {
+        heroSectImg.style.transform = `translateX(-${counter * 100}%)`
+    })
+}
